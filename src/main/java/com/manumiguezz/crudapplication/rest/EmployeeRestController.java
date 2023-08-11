fff5eb4f678f78ca5b81f20ce8b1c5eae75c2e1f
Manuel Miguez Lauría <manuelmiguezlauria@gmail.com>
2023-08-11 13:17:24 -0300

    update epmloyee method added

--- src/main/java/com/manumiguezz/crudapplication/rest/EmployeeRestController.java
+++ src/main/java/com/manumiguezz/crudapplication/rest/EmployeeRestController.java
@@ -43,5 +43,11 @@ public class EmployeeRestController {
         return dbEmployee;
     }
 
+    @PutMapping("/employees")
+    public Employee updateEmployee(@RequestBody Employee theEmployee) {
+        Employee dbEmployee = employeeService.save(theEmployee);
+        return dbEmployee;
+    }
+
 
 }
