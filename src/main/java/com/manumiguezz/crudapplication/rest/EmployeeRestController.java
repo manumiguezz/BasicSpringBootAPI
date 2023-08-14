f80329a7ef1a7be1ef9a4b9814de01eea05c2c11
Manuel Miguez Lauría <manuelmiguezlauria@gmail.com>
2023-08-14 18:03:43 -0300

    service layer added again

--- src/main/java/com/manumiguezz/crudapplication/rest/EmployeeRestController.java
+++ src/main/java/com/manumiguezz/crudapplication/rest/EmployeeRestController.java
@@ -1,6 +1,7 @@
 package com.manumiguezz.crudapplication.rest;
 
 import com.manumiguezz.crudapplication.entity.Employee;
+import com.manumiguezz.crudapplication.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,20 +11,18 @@ import java.util.List;
 @RequestMapping("/api")
 public class EmployeeRestController {
 
-    private Employee employeeService;
+    private EmployeeService employeeService;
 
     @Autowired
     public EmployeeRestController(EmployeeService theEmployeeService) {
         employeeService = theEmployeeService;
     }
 
-    // expose "/employees" and return a list of employees
     @GetMapping("/employees")
     public List<Employee> findAll() {
         return employeeService.findAll();
     }
 
-    // add mapping for GET /employees/{employeeId}
 
     @GetMapping("/employees/{employeeId}")
     public Employee getEmployee(@PathVariable int employeeId) {
@@ -37,13 +36,10 @@ public class EmployeeRestController {
         return theEmployee;
     }
 
-    // add mapping for POST /employees - add new employee
 
     @PostMapping("/employees")
     public Employee addEmployee(@RequestBody Employee theEmployee) {
 
-        // also just in case they pass an id in JSON ... set id to 0
-        // this is to force a save of new item ... instead of update
 
         theEmployee.setId(0);
 
@@ -51,8 +47,6 @@ public class EmployeeRestController {
 
         return dbEmployee;
     }
-
-    // add mapping for PUT /employees - update existing employee
 
     @PutMapping("/employees")
     public Employee updateEmployee(@RequestBody Employee theEmployee) {
@@ -62,14 +56,10 @@ public class EmployeeRestController {
         return dbEmployee;
     }
 
-    // add mapping for DELETE /employees/{employeeId} - delete employee
-
     @DeleteMapping("/employees/{employeeId}")
     public String deleteEmployee(@PathVariable int employeeId) {
 
         Employee tempEmployee = employeeService.findById(employeeId);
-
-        // throw exception if null
 
         if (tempEmployee == null) {
             throw new RuntimeException("Employee id not found - " + employeeId);
